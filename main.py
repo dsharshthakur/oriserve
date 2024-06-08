5e5092c33996a5ee9402c696568b137c133063fe
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-06-09 00:48:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class UserSentiment:
         answer: [extra charges positive, no stock negative, advisor/agent service positive]
 
         Also:
-        this is wrong : 
+        This output wrong : 
         "answer: [ease of use positive, value for money positive, advisor/agent service positive]"
         this is how the output should be:
         "[ease of use positive, value for money positive, advisor/agent service positive]"
