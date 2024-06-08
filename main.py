bd1deb20174bf091c97fca9c15bc6377dd5ce675
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-06-09 00:19:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ class UserSentiment:
         template = ''' 
         You are a great AI assistant which can easily perform "subtheme sentiments analysis" from a given  user review. 
         The text is {text} . Now, Identify all the subtheme sentiments from this text with highest accuracy, and don't 
-        miss any insight from the text also avoid giving irrelevant sentiments in output. Your answer should be strictly according to the given structure only.
+        miss any insight from the text also avoid giving irrelevant sentiments in output.
+        Your answer should be strictly according to the given structure only.
 
         For example 1:
         text: It was very straightforward and the garage was great. Hadn't even known about them before.
@@ -26,29 +27,31 @@ class UserSentiment:
         
         For example 2:
         text: Easy to use, also good price.
-        answer: [value for money positive]
+        answer should be: [value for money positive]
         
         For example 3:
         text: Staff with [REDACTED] went above and beyond to help me organise my booking and ordering my tires. The staff within the chosen garage were also quick and efficient. :).
-        answer : [ease of booking positive, length of fitting positive, advisor/agent service positive] 
+        answer should be: [ease of booking positive, length of fitting positive, advisor/agent service positive] 
         
         For example 4:
         text: Simple to order. Easy to book your appointment. Tyres turned up on time. Fitters did a great job.
-        answer:[delivery punctuality positive, ease of booking positive]
+        answer should be:[delivery punctuality positive, ease of booking positive]
         
         For example 5:
         text:Not a 10 as I found the garage a bit difficult to find
-        answer: [location negative]
+        answer should be: [location negative]
     
         For example 6:
         text:Good polite and professional service from Richard Beavers in Minehead.
         Bit of confusion intitially over when the tyres could be fitted but everything worked out in the end.
-        answer:[booking confusion negative, garage service positive]
+        answer should be:[booking confusion negative, garage service positive]
         
         For example 7:
         text: Great service, they had a supply issue with my original order and so upgraded to the next tyre up without charging the extra cost.
         Also they saw what could be an issue and highlighted it so that i could double check i had the correct order.
         answer: [extra charges positive, no stock negative, advisor/agent service positive]
+        '''
+        
         '''
         # prompt template for the llm - Instruction for the model
         prompt = PromptTemplate(template=template, input_variables=["text"])
