fbc60587c09f7816fac49fd1adf6ac60e6d93365
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-06-09 00:43:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ if submit_btn and review_text != "":
 
     # generating the AI - Response
     answer = model.generate(user_review=review_text)
+    st.write(answer)
 
     answer = re.sub(r'[\[\]]', "", string=answer).split(", ")
 
