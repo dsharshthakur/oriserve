aa1b23296ecebec89df2390610022039417155cf
dsharshthakur <harshsingh454545@gmail.com>
2024-06-08 23:19:21 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from main import UserSentiment
+from dotenv import load_dotenv
+import os
+import re
+
+# loading api key
+load_dotenv()
+google_key = os.getenv("GOOGLE_KEY")
+
+# custom CSS for submit button
+btn_style = '''<style>
+.stButton button {
+        background-color:#00CED1;
+        color: black;
+        transition: background-color 0.3s;
+        
+        }
+.stButton button:hover {
+    background-color:#4CE489;
+    border : 2px solid yellow;
+    color : black;
+    }
+    </style>
+'''
+
+st.markdown(btn_style, unsafe_allow_html=True)
+st.markdown("<h2 style= 'text-align:center'>User Sentiment Analysis</h2>", unsafe_allow_html=True)
+st.markdown("<br>",unsafe_allow_html=True)
+st.markdown("<br>",unsafe_allow_html=True)
+
+# take user input
+review_text = st.text_area(label="Insert the text here..")
+
+
+# layout for button
+btn_area = st.container()
+with btn_area:
+    col1, col2, col3 = btn_area.columns(3)
+    submit_btn = col2.button('Submit', use_container_width=True)
+
+
+# if button is active
+if submit_btn and review_text != "":
+    # model object
+    model = UserSentiment(api_key=google_key, llm_name="gemini-pro")
+
+    # generating the AI - Response
+    answer = model.generate(user_review=review_text)
+
+    answer = re.sub(r'[\[\]]', "", string=answer).split(", ")
+
+    # list containing positive response only
+    positive_response = [txt for txt in answer if 'positive' in txt]
+
+    # list containing negative response only
+    negative_response = [txt for txt in answer if 'negative' in txt]
+
+    # display response if positive response is not empty - Green color
+    if len(positive_response) != 0:
+        for sentiment in positive_response:
+            st.success(sentiment)
+
+    # display response if negative response is not empty - Red color
+    if len(negative_response) != 0:
+        for sentiment in negative_response:
+            st.error(sentiment)
+
---
+++ main.py
@@ -0,0 +1,65 @@
+# import required libraries
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+
+
+# class responsible for using gemini-pro to generate response
+class UserSentiment:
+    # initialization
+    def __init__(self, api_key, llm_name):
+        self.llm_api_key = api_key
+        self.llm_name = llm_name
+
+    # function responsible for generating response
+    def generate(self, user_review):
+        # LLM - (Google)
+        llm = GoogleGenerativeAI(model=self.llm_name, google_api_key=self.llm_api_key, model_kwrgs={"temperature": 0.2})
+
+        template = ''' 
+        You are a great AI assistant which can easily perform "subtheme sentiments analysis" from a given  user review. 
+        The text is {text} . Now, Identify all the subtheme sentiments from this text with highest accuracy, and don't 
+        miss any insight also avoid giving irrelevant sentiments.
+         Response should be strictly according to the given structure only.
+        
+        Some examples that shows how your output should be:
+        
+        For example 1:
+        text: It was very straightforward and the garage was great. Hadn't even known about them before.
+        answer: [garage service positive]
+        
+        For example 2:
+        text: Easy to use, also good price.
+        answer: [value for money positive]
+        
+        For example 3:
+        text: Staff with [REDACTED] went above and beyond to help me organise my booking and ordering my tires. The staff within the chosen garage were also quick and efficient. :).
+        answer : [ease of booking positive, length of fitting positive, advisor/agent service positive] 
+        
+        For example 4:
+        text: Simple to order. Easy to book your appointment. Tyres turned up on time. Fitters did a great job.
+        answer:[delivery punctuality positive, ease of booking positive]
+        
+        For example 5:
+        text:Not a 10 as I found the garage a bit difficult to find
+        answer: [location negative]
+    
+        For example 6:
+        text:Good polite and professional service from Richard Beavers in Minehead.
+        Bit of confusion intitially over when the tyres could be fitted but everything worked out in the end.
+        answer:[booking confusion negative, garage service positive]
+        
+        For example 7:
+        text: Great service, they had a supply issue with my original order and so upgraded to the next tyre up without charging the extra cost.
+        Also they saw what could be an issue and highlighted it so that i could double check i had the correct order.
+        answer: [extra charges positive, no stock negative, advisor/agent service positive]
+        '''
+        # prompt template for the llm - Instruction for the model
+        prompt = PromptTemplate(template=template, input_variables=["text"])
+
+        # combining the prompt and the model
+        chain = prompt | llm
+
+        # ai response
+        ai_response = chain.invoke({'text': user_review})
+
+        return ai_response
